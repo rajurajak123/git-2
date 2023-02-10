6d812339d37e61d46054feef41e0b7d2d8b3c730
rajurajak123 <rajurajak23998@gmail.com>
2023-02-10 13:24:02 +0530

    Prime number checking program

---
+++ prime.js
@@ -0,0 +1,15 @@
+let num=3;
+let count=0;
+for(let i=1;i<=num;i++)
+{
+    if(num%i==0)
+    {
+        count++;
+    }
+}
+if(count==2)
+{
+    console.log("prime number");
+}
+else
+console.log("Not prine number");
